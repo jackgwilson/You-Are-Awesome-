5c198c0ef2d655cfac565e38a229b3b860f0b1bb
Jack Wilson <JackWilson@jacks-macbook.bc.edu>
2018-08-28 14:47:02 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Jack Wilson on 8/28/18.
+//  Copyright © 2018 Jack Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
